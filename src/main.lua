e627e21d08b4c965f5535d169037c2b38752106d
Noman <noman@noman.land>
2023-11-11 23:07:58 -0500

    Connect worlds 👻

--- src/main.lua
+++ src/main.lua
@@ -13,14 +13,17 @@ function _init()
 end
 
 function _update()
-	frame=(frame+1)%30
+	frame=(frame+1)
 	poke(0x5f80, frame)
 	listen_for_reset()
 end
 
 function listen_for_reset()
 	-- Listen to the final array slot
-	if (peek(0x5fff) == 1) run()
+	if (peek(0x5fff) == 1) then
+		run()
+		print('why')
+	end
 end
 
 function _draw()
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log('hello');
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+const pins = ((self as any).pico8_gpio = new Proxy(new Array(120), {
+  set(target, p: string, frame) {
+    target[parseInt(p, 10)] = frame;
+    document.getElementById('frame')!.innerText = frame;
+    return true;
+  },
+}));
+
+const reload = () => {
+  pins[127] = 1;
+  setTimeout(() => {
+    pins[127] = 0;
+  }, 50);
+};
+
+document.getElementById('reload')!.addEventListener('click', reload);
+
+// setInterval(rerun, 5000);
